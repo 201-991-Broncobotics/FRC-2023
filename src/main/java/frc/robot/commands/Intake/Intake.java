5f8d8c54fbeba0d5557decc82bbee3d320cde651
shaankeole <skeole24@brophybroncos.org>
2023-02-22 11:46:41 -0700

    maybe a fix

--- src/main/java/frc/robot/commands/Intake/Intake.java
+++ src/main/java/frc/robot/commands/Intake/Intake.java
@@ -9,10 +9,12 @@ import static frc.robot.Constants.TuningConstants.*;
 
 public class Intake extends SequentialCommandGroup {
     public Intake(Claw claw, DoubleArm doubleArm) {
-        addRequirements(claw); // means that other functions are not allowed to access it
-        addCommands( // Let's see if this works lmao+
-            new SetArmPosition(doubleArm, intakePosition), 
-            new Intake_Subcommand(claw, doubleArm)
-        );
+        if (claw.getCurrent() == 0) { // THis might not work but I don't know until I test it
+            addRequirements(claw); // means that other functions are not allowed to access it
+            addCommands( // Let's see if this works lmao+
+                new SetArmPosition(doubleArm, intakePosition), 
+                new Intake_Subcommand(claw, doubleArm)
+            );
+        }
     }
 }
