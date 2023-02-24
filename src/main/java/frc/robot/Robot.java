1177c990a16f451e7bf6048690e12b101edb7dc4
Brophy-Robotics <broncobotics@brophybroncos.org>
2023-02-24 14:32:51 -0700

    fixed

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -92,9 +92,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopExit() {
-        System.out.print("Started Print"); // just in case it doesn't work
-        System.out.print(Variables.data); // just in case it doesn't work
-        System.out.print("Finished Print"); // just in case it doesn't work
+        System.out.print(Variables.data);
         SmartDashboard.putString("TeleOp Exit", "Successful");
         SmartDashboard.putString("Data", Variables.data);
     }
