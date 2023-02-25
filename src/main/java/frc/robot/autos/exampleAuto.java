5b448710df4450e05e922f32847bb3af766bb5ea
Rohan Godha <rgodha24@brophybroncos.org>
2023-02-25 15:08:21 -0700

    not v2

--- src/main/java/frc/robot/autos/exampleAuto.java
+++ src/main/java/frc/robot/autos/exampleAuto.java
@@ -3,16 +3,11 @@ package frc.robot.autos;
 import frc.robot.Constants;
 import frc.robot.subsystems.Swerve;
 
-import java.util.List;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
@@ -20,41 +15,23 @@ import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
-
-import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 
 public class exampleAuto extends SequentialCommandGroup {
     public exampleAuto(Swerve s_Swerve) {
-        TrajectoryConfig config =
-            new TrajectoryConfig(
-                    Constants.AutoConstants.kMaxSpeedMetersPerSecond,
-                    Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-                .setKinematics(Constants.BaseFalconSwerve.swerveKinematics);
-
-        // An example trajectory to follow.  All units in meters.
-        Trajectory exampleTrajectory =
-            TrajectoryGenerator.generateTrajectory(
-                // Start at the origin facing the +X direction
-                new Pose2d(0, 0, new Rotation2d(0)),
-                // Pass through these two interior waypoints, making an 's' curve path
-                List.of(
-                    new Translation2d(1, 1), 
-                    new Translation2d(2, -1)),
-                    
-                // End 3 meters straight ahead of where we started, facing forward
-                    new Pose2d(3, 0, new Rotation2d(180)),
-                config);
 
         String trajectoryJSON = "paths/test.wpilib.json";
-        Trajectory trajectory = new Trajectory();
+        final Trajectory trajectory;
+
+        Trajectory temp = new Trajectory();
 
         try {
             Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
-            trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
-         } catch (IOException ex) {
+            temp = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+        } catch (IOException ex) {
             System.out.println("Sad because Unable to open trajectory: " + trajectoryJSON + " " + ex.getStackTrace());
-         }
+        }
+         
+        trajectory = temp;
 
         var thetaController =
             new ProfiledPIDController(
