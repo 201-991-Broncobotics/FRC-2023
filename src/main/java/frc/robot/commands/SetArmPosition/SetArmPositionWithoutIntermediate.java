c53749792f9bd02787c0dc0ab71ae0f3ce7d81ff
shaankeole <skeole24@brophybroncos.org>
2023-03-01 12:27:18 -0700

    no more smartdashboard stuff it works now

--- src/main/java/frc/robot/commands/SetArmPosition/SetArmPositionWithoutIntermediate.java
+++ src/main/java/frc/robot/commands/SetArmPosition/SetArmPositionWithoutIntermediate.java
@@ -2,8 +2,6 @@ package frc.robot.commands.setArmPosition;
 
 import java.util.function.BooleanSupplier;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.DoubleArm;
 
@@ -21,13 +19,9 @@ public class SetArmPositionWithoutIntermediate extends SequentialCommandGroup {
     public SetArmPositionWithoutIntermediate(DoubleArm doubleArm, double[] target_angles, BooleanSupplier stopSup) {
         addRequirements(doubleArm);
         addCommands(
-            new InstantCommand(() -> SmartDashboard.putString("Status", "Set Max Distal")),
             new SetMaxDistalPosition(doubleArm, target_angles[0], stopSup), 
-            new InstantCommand(() -> SmartDashboard.putString("Status", "Set Proximal")),
             new SetProximalPosition(doubleArm, target_angles[0], stopSup), 
-            new InstantCommand(() -> SmartDashboard.putString("Status", "Set Final Distal")),
-            new SetDistalPosition(doubleArm, target_angles[1], stopSup),
-            new InstantCommand(() -> SmartDashboard.putString("Status", "Finished Setting Arm Target"))
+            new SetDistalPosition(doubleArm, target_angles[1], stopSup)
         );
     }
 }
