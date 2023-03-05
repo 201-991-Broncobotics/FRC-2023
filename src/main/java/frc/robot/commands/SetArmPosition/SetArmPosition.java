ceb3f5a92f5699a7fb45ed02aa44c50116797e4f
shaankeole <skeole24@brophybroncos.org>
2023-03-04 20:56:04 -0700

    better setarmposition

--- src/main/java/frc/robot/commands/SetArmPosition/SetArmPosition.java
+++ src/main/java/frc/robot/commands/SetArmPosition/SetArmPosition.java
@@ -1,7 +1,7 @@
 package frc.robot.commands.setArmPosition;
 
 import frc.robot.subsystems.DoubleArm;
-import static frc.robot.Constants.TuningConstants.*;
+import static frc.robot.Constants.DoubleArmConstants.*;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
@@ -10,8 +10,8 @@ public class SetArmPosition extends SequentialCommandGroup {
     public SetArmPosition(DoubleArm doubleArm, double[] position) {
         addRequirements(doubleArm);
         addCommands(
-            new SetMaxDistalPosition(doubleArm, intermediatePositionAngles[0]), 
-            new SetProximalPosition(doubleArm, intermediatePositionAngles[0]), 
+            new SetMaxDistalPosition(doubleArm, Math.min(90, max_second_angle) - 180 + min_difference), 
+            new SetProximalPosition(doubleArm, Math.min(90, max_second_angle) - 180 + min_difference), 
             new SetMaxDistalPosition(doubleArm, position[0]), 
             new SetProximalPosition(doubleArm, position[0]), 
             new SetDistalPosition(doubleArm, position[1])
