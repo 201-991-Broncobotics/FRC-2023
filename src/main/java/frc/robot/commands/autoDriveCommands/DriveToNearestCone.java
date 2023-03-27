f36e1719f5145c7be8632cb720a7f6b1dc61eeb2
shaankeole <skeole24@brophybroncos.org>
2023-03-26 18:26:28 -0700

    setup for auto driving

--- src/main/java/frc/robot/commands/autoDriveCommands/DriveToNearestCone.java
+++ src/main/java/frc/robot/commands/autoDriveCommands/DriveToNearestCone.java
@@ -1,5 +1,16 @@
 package frc.robot.commands.autoDriveCommands;
 
 public class DriveToNearestCone {
-    
+    // Y Values:
+    // Short Left: 4.98
+    // Short Right: 3.87
+    // Center Left: 3.3
+    // Center Right: 2.18
+    // Long Left: 1.63
+    // Long Right: 0.5
+
+    // Intial X: 2.1
+    // Final X: 1.85
+
+    // Only do it if we're already in the zone. That is, (x, y) < (2.85, 5.5). Note this has to be adjusted for red
 }
---
+++ src/main/java/frc/robot/commands/autoDriveCommands/DriveToSingleSubstation.java
@@ -0,0 +1,8 @@
+package frc.robot.commands.autoDriveCommands;
+
+public class DriveToSingleSubstation {
+    // 14.1, 7.08
+    // Then go to y = 7.5
+
+    // Only go if above (10.3, 6.5); same for double substation
+}
